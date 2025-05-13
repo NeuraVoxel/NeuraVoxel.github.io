0f6ef41e4311c993728f5f7fcb57a35f033b19fd
ue007 <82075874+ue007@users.noreply.github.com>
2025-05-13 16:36:22 +0800

    feat(blog): add new blog post on parking data visualization in RVIZ
    Add a new blog post titled "Autonomous Driving: Parking Data Visualization in RVIZ" along with supporting images. The post discusses the core aspects of parking perception in autonomous systems, including static and dynamic environment perception, and provides insights into future advancements in the field.

--- src/blogs/index.ts
+++ src/blogs/index.ts
@@ -3,6 +3,7 @@ import deepLearningFor3DPointCloud from './deep-learning-for-3d-point-cloud';
 import gazeboFireDetection from './03-gazebo-fire-detection';
 import slamMappingAndroid from './04-slam-mapping-android';
 import adRvizDriving from './05-ad-rviz-driving';
+import adRvizParking from './06-ad-rviz-parking';
 
 // 博客文章数据映射
 const blogPostsData: { [key: string]: any } = {
@@ -11,6 +12,7 @@ const blogPostsData: { [key: string]: any } = {
     'gazeboFireDetection': gazeboFireDetection,
     'slamMappingAndroid': slamMappingAndroid,
     'adRvizDriving': adRvizDriving,
+    'adRvizParking': adRvizParking,
 };
 
 export default blogPostsData;
